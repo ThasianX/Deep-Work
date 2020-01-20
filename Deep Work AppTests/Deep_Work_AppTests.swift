2b030c5700ebb3f582f38c006159917568f14aee
Kevin Li <kevinli29033@gmail.com>
2020-01-20 17:01:27 -0500

    Initial Commit

---
+++ Deep Work AppTests/Deep_Work_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Deep_Work_AppTests.swift
+//  Deep Work AppTests
+//
+//  Created by Kevin Li on 1/20/20.
+//  Copyright © 2020 Kevin Li. All rights reserved.
+//
+
+import XCTest
+@testable import Deep_Work_App
+
+class Deep_Work_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
